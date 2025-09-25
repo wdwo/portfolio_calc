ee4bdf016a7100d63f78eebd1112756b32bdd045
Daniel Wong <danielwg@gmail.com>
2025-09-25 14:36:04 +0800

    added env variables to config.py

--- config.py
+++ config.py
@@ -16,26 +16,26 @@ class Settings:
     DB_FULLPATH = DB_PATH / DB_FILENAME
 
     # Table names
-    TRANSACTIONS_TABLE = 'equity_transactions'
-    REALIZED_PNL_TABLE = 'realized_pnl'
-    UNREALIZED_PNL_TABLE = 'unrealized_pnl'
+    TRANSACTIONS_TABLE = os.getenv('EQUITY_TRANSACTIONS_TABLE','equity_transactions')
+    REALIZED_PNL_TABLE = os.getenv('REALIZED_PNL_TABLE','realized_pnl')
+    UNREALIZED_PNL_TABLE = os.getenv('UNREALIZED_PNL_TABLE','unrealized_pnl')
 
     # Column names
-    TICKER_COL = 'ticker'
-    QUANTITY_COL = 'quantity'
-    PRICE_COL = 'transacted_price'
-    DATE_COL = 'transaction_date' # Assuming this column contains a date/datetime value
-    FX_COL = 'fx'
+    TICKER_COL = os.getenv('TICKER_COL','ticker')
+    QUANTITY_COL = os.getenv('QUANTITY_COL','quantity')
+    PRICE_COL = os.getenv('PRICE_COL','transacted_price')
+    DATE_COL = os.getenv('DATE_COL','transaction_date') # Assuming this column contains a date/datetime value
+    FX_COL = os.getenv('FX_COL','fx')
 
     # Current stock price table and columns
-    STOCK_DATA_TABLE = 'stock_data'
-    TICKER_COL_STOCK_DATA = 'ticker_base'
-    PRICE_COL_STOCK_DATA = 'last_price'
+    STOCK_DATA_TABLE = os.getenv('STOCK_DATA_TABLE','stock_data')
+    TICKER_COL_STOCK_DATA = os.getenv('TICKER_COL_STOCK_DATA','ticker_base')
+    PRICE_COL_STOCK_DATA = os.getenv('PRICE_COL_STOCK_DATA','last_price')
 
     # FX rate table and columns
-    FX_DATA_TABLE = 'fx_data'
-    CCY_COL_FX = 'CCY1'
-    RATE_COL_FX = 'rate'
+    FX_DATA_TABLE = os.getenv('FX_DATA_TABLE','fx_data')
+    CCY_COL_FX = os.getenv('CCY_COL_FX','CCY1')
+    RATE_COL_FX = os.getenv('RATE_COL_FX','rate')
     
     # Transaction table currency column
-    CCY_COL_TRANS = 'ccy'
+    CCY_COL_TRANS = os.getenv('CCY_COL_TRANS','ccy')
